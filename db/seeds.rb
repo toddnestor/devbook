b77cda8fcb315840e136dbdb015cd01160e6623d
Todd Nestor <todd.nestor@gmail.com>
2016-11-02 10:02:55 -0700

    added luke and leia seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  fname: 'Luke',
+  lname: 'Skywalker',
+  username: 'luke',
+  email: 'jedi@rebellion.org',
+  hometown: 'Tatooine',
+  password: '123123',
+  demo: true
+)
+
+User.create(
+  fname: 'Leia',
+  lname: 'Organa',
+  username: 'leia',
+  email: 'princess@rebellion.org',
+  hometown: 'Alderaan',
+  password: '123123',
+  demo: true
+)
