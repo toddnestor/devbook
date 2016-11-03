4716667f633fbaeada75a3796e777c97c8c7f38b
Todd Nestor <todd.nestor@gmail.com>
2016-11-03 14:47:30 -0700

    edit profile page is much prettier now

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,3 +1,3 @@
 json.extract! user, :id, :fname, :lname, :username, :email, :hometown, :works_at,
       :lives_at, :intro, :tagline, :avatar_url, :cover_image_url, :birthday,
-      :gender
+      :gender, :relationship_status
--- frontend/components/profile/edit_profile_form.jsx
+++ frontend/components/profile/edit_profile_form.jsx
@@ -11,6 +11,7 @@ class EditProfileForm extends React.Component {
     this.state.errors = {};
     this.state.password = '';
     this.state.password_confirmation = '';
+    this.state.editingTab = 'details';
 
     if( this.state.birthday ) {
       let birthday = new Date(this.props.user.birthday);
@@ -209,8 +210,15 @@ class EditProfileForm extends React.Component {
     return years;
   }
 
+  updateTab(tab) {
+    return e => {
+      e.preventDefault();
+      this.setState({editingTab: tab});
+    }
+  }
+
   render() {
-    let {errors} = this.state;
+    let {errors, editingTab} = this.state;
 
     return (
       <div className="sign-up-form">
@@ -218,102 +226,139 @@ class EditProfileForm extends React.Component {
         <div className="subtitle"> </div>
 
         <form onSubmit={this.handleSubmit.bind(this)}>
-          <div className="row">
-            <div className={errors['fname'] ? 'col-sm-6 form-group has-error' : 'col-sm-6 form-group'}>
-              <input type="text" className="form-control" onChange={this.update('fname').bind(this)} value={this.state.fname} placeholder="First name" />
-              <span className="help-block" style={{display: errors['fname'] ? 'block' : 'none'}}>{errors['fname']}</span>
-            </div>
-            <div className={errors['lname'] ? 'col-sm-6 form-group has-error' : 'col-sm-6 form-group'}>
-              <input type="text" className="form-control" onChange={this.update('lname').bind(this)} value={this.state.lname} placeholder="Last name" />
-              <span className="help-block" style={{display: errors['lname'] ? 'block' : 'none'}}>{errors['lname']}</span>
-            </div>
-            <div className={errors['username'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'}>
-              <input type="text" className="form-control" onChange={this.update('username').bind(this)} value={this.state.username} placeholder="Username" />
-              <span className="help-block" style={{display: errors['username'] ? 'block' : 'none'}}>{errors['username']}</span>
-            </div>
-            <div className={errors['email'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'}>
-              <input type="text" className="form-control" onChange={this.update('email').bind(this)} value={this.state.email} placeholder="E-mail address" />
-              <span className="help-block" style={{display: errors['email'] ? 'block' : 'none'}}>{errors['email']}</span>
-            </div>
-            <div className={errors['password'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'}>
-              <input type="password" className="form-control" onChange={this.update('password').bind(this)} value={this.state.password} placeholder="New password" />
-              <span className="help-block text-info">Leave blank to keep current password</span>
-              <span className="help-block" style={{display: errors['password'] ? 'block' : 'none'}}>{errors['password']}</span>
-            </div>
-            <div className={errors['password_confirmation'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'} style={{display: this.state.password ? 'block' : 'none'}}>
-              <input type="password" className="form-control" onChange={this.update('password_confirmation').bind(this)} value={this.state.password_confirmation} placeholder="Confirm password" />
-              <span className="help-block" style={{display: errors['password_confirmation'] ? 'block' : 'none'}}>{errors['password_confirmation']}</span>
-            </div>
-            <div className={errors['birthday'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'}>
-              <label>Birthday</label>
-              <div className="birthday-selector">
-                <select onChange={this.update('birthday_month').bind(this)} value={this.state.birthday_month}>
-                  <option value="0">Month</option>
-                  {
-                    Object.keys(this.months()).map( n => (
-                      <option key={n} value={n}>{this.months()[n]}</option>
-                    ))
-                  }
-                </select>
-                <select onChange={this.update('birthday_day').bind(this)} value={this.state.birthday_day}>
-                  <option value="0">Day</option>
-                  {
-                    this.monthDays().map( n => (
-                      <option key={n} value={n}>{n}</option>
-                    ))
-                  }
-                </select>
-                <select onChange={this.update('birthday_year').bind(this)} value={this.state.birthday_year}>
-                  <option value="0">Year</option>
-                    {
-                      this.years().map( n => (
-                        <option key={n} value={n}>{n}</option>
-                      ))
-                    }
-                </select>
-              </div>
-              <span className="help-block" style={{display: errors['birthday'] ? 'block' : 'none'}}>{errors['birthday']}</span>
-            </div>
-            <div className="col-sm-12 gender-selector">
-              <label className="radio-inline">
-                <input type="radio" checked={this.state.gender == 'male'} value="male" onChange={this.update('gender').bind(this)} /> Male
-                </label>
-                <label className="radio-inline">
-                  <input type="radio" checked={this.state.gender == 'female'} value="female" onChange={this.update('gender').bind(this)} /> Female
+          <ul className="nav nav-tabs">
+            <li className={editingTab == 'details' ? 'active' : ''} onClick={this.updateTab('details').bind(this)}>
+              <a href="#">Personal Details</a>
+            </li>
+            <li className={editingTab == 'intro' ? 'active' : ''} onClick={this.updateTab('intro').bind(this)}>
+              <a href="#">Introduction</a>
+            </li>
+            <li className={editingTab == 'locations' ? 'active' : ''} onClick={this.updateTab('locations').bind(this)}>
+              <a href="#">Locations</a>
+            </li>
+            <li className={editingTab == 'account' ? 'active' : ''} onClick={this.updateTab('account').bind(this)}>
+              <a href="#">Account</a>
+            </li>
+          </ul>
+          <div className="tab-content">
+            <div className={editingTab == 'details' ? "tab-pane fade in active" : "tab-pane fade"}>
+              <div className="row">
+                <div className={errors['fname'] ? 'col-sm-6 form-group has-error' : 'col-sm-6 form-group'}>
+                  <label htmlFor="fname">First Name</label>
+                  <input type="text" id="fname" className="form-control" onChange={this.update('fname').bind(this)} value={this.state.fname} placeholder="First name" />
+                  <span className="help-block" style={{display: errors['fname'] ? 'block' : 'none'}}>{errors['fname']}</span>
+                </div>
+                <div className={errors['lname'] ? 'col-sm-6 form-group has-error' : 'col-sm-6 form-group'}>
+                  <label htmlFor="lname">Last Name</label>
+                  <input type="text" id="lname" className="form-control" onChange={this.update('lname').bind(this)} value={this.state.lname} placeholder="Last name" />
+                  <span className="help-block" style={{display: errors['lname'] ? 'block' : 'none'}}>{errors['lname']}</span>
+                </div>
+                <div className={errors['birthday'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'}>
+                  <label>Birthday</label>
+                  <div className="birthday-selector">
+                    <select onChange={this.update('birthday_month').bind(this)} value={this.state.birthday_month}>
+                      <option value="0">Month</option>
+                      {
+                        Object.keys(this.months()).map( n => (
+                          <option key={n} value={n}>{this.months()[n]}</option>
+                        ))
+                      }
+                    </select>
+                    <select onChange={this.update('birthday_day').bind(this)} value={this.state.birthday_day}>
+                      <option value="0">Day</option>
+                      {
+                        this.monthDays().map( n => (
+                          <option key={n} value={n}>{n}</option>
+                        ))
+                      }
+                    </select>
+                    <select onChange={this.update('birthday_year').bind(this)} value={this.state.birthday_year}>
+                      <option value="0">Year</option>
+                        {
+                          this.years().map( n => (
+                            <option key={n} value={n}>{n}</option>
+                          ))
+                        }
+                    </select>
+                  </div>
+                  <span className="help-block" style={{display: errors['birthday'] ? 'block' : 'none'}}>{errors['birthday']}</span>
+                </div>
+                <div className="col-sm-12 form-group">
+                  <label htmlFor="relationship_status">Relationship Status</label>
+                  <input id="relationship_status" type="text" className="form-control" onChange={this.update('relationship_status').bind(this)} value={this.state.relationship_status} placeholder="Relationship Status..." />
+                </div>
+                <div className="col-sm-12 gender-selector">
+                  <label style={{display: 'block'}}>Gender</label>
+                  <label className="radio-inline">
+                    <input type="radio" checked={this.state.gender == 'male'} value="male" onChange={this.update('gender').bind(this)} /> Male
+                  </label>
+                  <label className="radio-inline">
+                    <input type="radio" checked={this.state.gender == 'female'} value="female" onChange={this.update('gender').bind(this)} /> Female
                   </label>
                   <label className="radio-inline">
                     <input type="radio" checked={this.state.gender == 'other'} value="other" onChange={this.update('gender').bind(this)} /> Other
-                    </label>
-            </div>
-            <div className="col-sm-12 form-group">
-              <label htmlFor="tagline">Tagline</label>
-              <input id="tagline" type="text" className="form-control" onChange={this.update('tagline').bind(this)} value={this.state.tagline} placeholder="Tagline" />
-            </div>
-            <div className="col-sm-12 form-group">
-              <label htmlFor="intro">Intro</label>
-              <Textarea id="intro" className="form-control" onChange={this.update('intro').bind(this)} value={this.state.intro} placeholder="Intro..."></Textarea>
-            </div>
-            <div className="col-sm-12 form-group">
-              <label htmlFor="relationship_status">Relationship Status</label>
-              <input id="relationship_status" type="text" className="form-control" onChange={this.update('relationship_status').bind(this)} value={this.state.relationship_status} placeholder="Relationship Status..." />
-            </div>
-            <div className="col-sm-12 form-group">
-              <label htmlFor="hometown">Hometown</label>
-              <input type="text" id="hometown" className="form-control" onChange={this.update('hometown').bind(this)} value={this.state.hometown} placeholder="Hometown" />
-            </div>
-            <div className="col-sm-12 form-group">
-              <label htmlFor="works_at">Works at</label>
-              <input id="works_at" type="text" className="form-control" onChange={this.update('works_at').bind(this)} value={this.state.works_at} placeholder="Works at..." />
-            </div>
-            <div className="col-sm-12 form-group">
-              <label htmlFor="lives_at">Lives at</label>
-              <input id="lives_at" type="text" className="form-control" onChange={this.update('lives_at').bind(this)} value={this.state.lives_at} placeholder="Lives at..." />
-            </div>
+                  </label>
                 </div>
-                <button disabled={this.hasErrors()} className="btn btn-success btn-lg green-button sign-up-button">Update Profile</button>
-              </form>
+              </div>
             </div>
-          );
+            <div className={editingTab == 'intro' ? "tab-pane fade in active" : "tab-pane fade"}>
+              <div className="row">
+                <div className="col-sm-12 form-group">
+                  <label htmlFor="tagline">Tagline</label>
+                  <input id="tagline" type="text" className="form-control" onChange={this.update('tagline').bind(this)} value={this.state.tagline} placeholder="Tagline" />
+                </div>
+                <div className="col-sm-12 form-group">
+                  <label htmlFor="intro">Intro</label>
+                  <Textarea id="intro" className="form-control" onChange={this.update('intro').bind(this)} value={this.state.intro} placeholder="Intro..."></Textarea>
+                </div>
+              </div>
+            </div>
+            <div className={editingTab == 'locations' ? "tab-pane fade in active" : "tab-pane fade"}>
+              <div className="row">
+                <div className="col-sm-12 form-group">
+                  <label htmlFor="hometown">Hometown</label>
+                  <input type="text" id="hometown" className="form-control" onChange={this.update('hometown').bind(this)} value={this.state.hometown} placeholder="Hometown" />
+                </div>
+                <div className="col-sm-12 form-group">
+                  <label htmlFor="works_at">Works at</label>
+                  <input id="works_at" type="text" className="form-control" onChange={this.update('works_at').bind(this)} value={this.state.works_at} placeholder="Works at..." />
+                </div>
+                <div className="col-sm-12 form-group">
+                  <label htmlFor="lives_at">Lives at</label>
+                  <input id="lives_at" type="text" className="form-control" onChange={this.update('lives_at').bind(this)} value={this.state.lives_at} placeholder="Lives at..." />
+                </div>
+              </div>
+            </div>
+            <div className={editingTab == 'account' ? "tab-pane fade in active" : "tab-pane fade"}>
+              <div className="row">
+                <div className={errors['username'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'}>
+                  <label htmlFor="username">Username</label>
+                  <input htmlFor="username" type="text" className="form-control" onChange={this.update('username').bind(this)} value={this.state.username} placeholder="Username" />
+                  <span className="help-block" style={{display: errors['username'] ? 'block' : 'none'}}>{errors['username']}</span>
+                </div>
+                <div className={errors['email'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'}>
+                  <label htmlFor="email">E-mail</label>
+                  <input id="email" type="text" className="form-control" onChange={this.update('email').bind(this)} value={this.state.email} placeholder="E-mail address" />
+                  <span className="help-block" style={{display: errors['email'] ? 'block' : 'none'}}>{errors['email']}</span>
+                </div>
+                <div className={errors['password'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'}>
+                  <label htmlFor="password">New Password</label>
+                  <input id="password" type="password" className="form-control" onChange={this.update('password').bind(this)} value={this.state.password} placeholder="New password" />
+                  <span className="help-block text-info">Leave blank to keep current password</span>
+                  <span className="help-block" style={{display: errors['password'] ? 'block' : 'none'}}>{errors['password']}</span>
+                </div>
+                <div className={errors['password_confirmation'] ? 'col-sm-12 form-group has-error' : 'col-sm-12 form-group'} style={{display: this.state.password ? 'block' : 'none'}}>
+                  <label htmlFor="password_confirmation">Confirm Password</label>
+                  <input id="password_confirmation" type="password" className="form-control" onChange={this.update('password_confirmation').bind(this)} value={this.state.password_confirmation} placeholder="Confirm password" />
+                  <span className="help-block" style={{display: errors['password_confirmation'] ? 'block' : 'none'}}>{errors['password_confirmation']}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button disabled={this.hasErrors()} className="btn btn-success btn-lg green-button sign-up-button">Update Profile</button>
+        </form>
+      </div>
+    );
   }
 };
 
