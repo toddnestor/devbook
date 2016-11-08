f0d84dc0ecf58373d881747be3025c1116eae7ec
Todd Nestor <todd.nestor@gmail.com>
2016-11-08 09:16:40 -0800

    you can now comment on a status if either the user or wall user is friends with you

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'paperclip', '~> 5.0.0'
 gem 'aws-sdk', '~> 2.6.14'
 gem 'state_machines-activerecord'
+gem 'kaminari'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/status.rb
+++ app/models/status.rb
@@ -2,6 +2,11 @@ class Status < ApplicationRecord
   after_create :create_activity
 
   belongs_to :user
+  belongs_to :wall_user,
+    primary_key: :id,
+    foreign_key: :wall_id,
+    class_name: :User
+
   has_many :attachments, as: :attachable, dependent: :destroy
 
   has_many :media_items, through: :attachments
@@ -14,6 +19,8 @@ class Status < ApplicationRecord
   end
 
   def can_comment?(other_user)
-    self.user == other_user || self.user.are_we_friends?(other_user)
+    return true if self.user == other_user || self.user.are_we_friends?(other_user)
+    return true if self.wall_user == other_user || self.wall_user.are_we_friends?(other_user)
+    false
   end
 end
